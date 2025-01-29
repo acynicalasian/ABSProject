e5a49f35bcb5653964f63f5f73695290cf33fd87
Arthur Kim <arthur.kim@ucla.edu>
2025-01-28 17:47:47 -0800

    Looks like I've finished a function to grab a list of branches so far.

--- TmoTask/Controllers/PerformanceReportController.cs
+++ TmoTask/Controllers/PerformanceReportController.cs
@@ -23,19 +23,34 @@ namespace TmoTask.Controllers
         public IActionResult GetBranches()
         {
             /**************************************************************************************
-            /   Read dynamically from orders.csv; in a prod env, orders.csv could've changed from 
-            /   the last time we read from it.
-            /
-            /   NOTE: This code forces the database to be reread every time it's run. This is
-            /   probably bad for production level code.
-            **************************************************************************************/
+             * Returns an HTTP status 200 code and a JSON containing a list of the branch names in
+             * the CSV file.
+             * Read dynamically from orders.csv; in a prod env, orders.csv could've changed from 
+             * the last time we read from it.
+             * 
+             * NOTE: This code forces the database to be reread every time it's run. This is 
+             * probably bad for production level code.
+             * ***********************************************************************************/
             
             HashSet<string> branchnames = new HashSet<string>();
             using (var reader = new StreamReader("../orders.csv"))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-
+                var parsed = csv.GetRecords<CsvHeaders>();
+                foreach (var line in parsed)
+                {
+                    branchnames.Add(line.Branch);
+                }
             }
+            string[] arr = new string[branchnames.Count];
+            branchnames.CopyTo(arr);
+            var result = new
+            {
+                list = arr
+            };
+            return Ok(result);
         }
+
+        // Returns 
     }
 }
