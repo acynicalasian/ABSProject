b61a63f82892652e209c3ad2680857eb18da6f9a
Arthur Kim <arthur.kim@ucla.edu>
2025-01-31 10:51:52 -0800

    Refactored GetBranches() to its functionality into two stages and validating the input CSV file. Added functionality to avoid having to reread the database when grabbing top sellers; added validation of CSV data.
    Added unit tests using MSTestv2.
    
    Honestly I should've split these changes into multiple commits but it is what it is.
    
    Waiting on confirmation on price field formatting constraints.

--- TmoTask/Controllers/PerformanceReportController.cs
+++ TmoTask/Controllers/PerformanceReportController.cs
@@ -1,8 +1,6 @@
 using CsvHelper;
-using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace TmoTask.Controllers
@@ -11,42 +9,156 @@ namespace TmoTask.Controllers
     [Route("[controller]/[action]")]
     public class PerformanceReportController : ControllerBase
     {
-        private string CSV_SRC_PATH = "../orders.csv";
+        private string CSV_SRC_PATH;
+        public PerformanceReportController() : base()
+        {
+            this.CSV_SRC_PATH = "../orders.csv";
+        }
+
+        // Probably a much better way to do this, but I'm just hacking together unit tests with
+        // MSTestv2, and I have no clue what kind of mess I'll create if the automatically
+        // generated PerformanceReportControllerTests class inherits from this class; otherwise
+        // I'd just make CSV_SRC_PATH protected or something.
+        //
+        // This violates naming conventions but it's not meant to be used outside test cases so it
+        // should be fine? I'd assume C# will still let this compile and just output a warning.
+        public void _setcsvpath(string s)
+        {
+            CSV_SRC_PATH = s;
+            return;
+        }
 
         private class CsvHeaders
         {
             public required string Seller { get; set; }
             public required string Product { get; set; }
-            public required string Price { get; set; }
+            public required double Price { get; set; }
             public required string OrderDate { get; set; }
             public required string Branch { get; set; }
         }
+
+        private class HoldJson
+        {
+            public string[]? list { get; set; }
+        }
+
+        // Store the processed CSV's data here to avoid having to use CsvHelper to reread
+        // orders.csv, unless a user explicitly states they want to refresh the database.
+        private IEnumerable<CsvHeaders>? _processeddata;
+
+        private string[]? _processedbranchlist;
 
         [HttpGet]
         public OkObjectResult GetBranches()
         {
             /**************************************************************************************
              * Returns an HTTP status 200 code and a JSON containing a list of the branch names in
-             * the CSV file.
+             * the CSV file. 
+             * 
+             * Stores the processed CSV file as a member object to avoid having to reprocess it.
+             * 
              * Read dynamically from orders.csv; in a prod env, orders.csv could've changed from 
              * the last time we read from it.
-             * 
-             * This code assumes that the orders.csv "database" originates from server-side and is
-             * thus trusted, safe, correctly formatted code, hence why it returns an
-             * OkObjectResult.
-             * 
-             * NOTE: This code forces the database to be reread every time it's run. This is 
-             * probably bad for production level code. I'm assuming enterprise-level solutions
-             * would likely cache/memoize the results of this function, with some sort of boolean
-             * option/parameter to force a fresh read of the orders.csv "database".
              * ***********************************************************************************/
 
             // First, ensure that the input file orders.csv isn't malformed. Read the first line
             // of the file.
-            //
+            this.ParseCsvHeader();
+
+            // Read each line from the CSV file. Return a JSON-formatted string with one key "list"
+            // whose value is an array of strings corresponding to the branch names.
+            var branchnames = this.ProcessCSVAndExtractBranchnames();
+            string[] arr = new string[branchnames.Count];
+            branchnames.CopyTo(arr);
+            this._processedbranchlist = arr;
+            var result = new
+            {
+                list = arr
+            };
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{branchname}/{numSellers}/{refresh=false}")]
+        public IActionResult GetTopSellers(string branchname, int numSellers, bool refresh=false)
+        {
+            /**************************************************************************************
+             * Returns an HTTP status code 200 and a corresponding JSON-format string listing the
+             * top numSellers sellers at branch named branchname. If no entries in orders.csv
+             * correspond to branchname, returns code 404 with an error message. If numSellers
+             * exceeds the number of sellers corresponding to branchname, return all sellers at
+             * branchname.
+             * 
+             * If refresh is true, this forces the entire CSV database to be reprocessed. Else,
+             * search the data stored in the member variable set by GetBranches().
+             * 
+             * NOTE: From my understanding of the technical requirements, I could easily move the
+             *       the functionality of this code into the GetBranches() function, and I suspect
+             *       that was why the source file provided as a template provided a single Get()
+             *       function that I could fill out. However, in a live environment, grabbing a
+             *       list of branches and grabbing the top sellers from each branch aren't two
+             *       functionalities that always occur together; what if someone wanted to grab the
+             *       list of branches and then sort by product instead, for example?
+             * ***********************************************************************************/
+
+            // If the value is null, we haven't even procesed the database yet. Do that first and
+            // go from there. Or, if we want a refresh, reprocess the CSV file first.
+            if (_processeddata == null || refresh || _processedbranchlist == null)
+            {
+                this.GetBranches();
+                return this.GetTopSellers(branchname, numSellers, false);
+            }
+
+            if (!this._processedbranchlist.Contains(branchname))
+            {
+                return NotFound($"No branch named {branchname} was found.");
+            }
+
+            if (numSellers < 1) return BadRequest("Number of sellers cannot be less than 1.");
+
+            // Accumulate totals for each seller.
+            var sellerTotals = new Dictionary<string, double>();
+            foreach(var line in this._processeddata)
+            {
+                if (line.Branch == branchname)
+                {
+                    if (sellerTotals.ContainsKey(line.Seller))
+                        sellerTotals[line.Seller] += line.Price;
+                    else
+                        sellerTotals.Add(line.Seller, line.Price);
+                }
+            }
+
+            // Swap key and value pairs in our sellerTotals into a new dict. Copy the keys in the
+            // new dict to an array, and then sort it high to low. We can now use the values of
+            // the sorted array to quickly access key-value pairs in the reversed dict to get an
+            // array of the sellars ordered from high to low sales.
+            var reverseddict = new Dictionary<double, string>();
+            foreach(var key in sellerTotals.Keys)
+            {
+                reverseddict.Add(sellerTotals[key], key);
+            }
+            double[] sortedvalues = new double[reverseddict.Count];
+            reverseddict.Keys.CopyTo(sortedvalues, 0);
+            Array.Sort(sortedvalues, (a,b) => Math.Sign(b-a));
+            string[] sellersranked = new string[sortedvalues.Length];
+            for (int i = 0; i < sortedvalues.Length; i++)
+                sellersranked[i] = reverseddict[sortedvalues[i]];
+            int n = numSellers;
+            var objtojson = new
+            {
+                ranking = (n >= sortedvalues.Length) ? sellersranked : sellersranked[0..n],
+                sellertotal = (n >= sortedvalues.Length) ? sortedvalues : sortedvalues[0..n]
+            };
+            // Ordering of the arrays lines up, so ranking[i]'s total is sellartotal[i].
+            return Ok(objtojson);
+        }
+
+        private void ParseCsvHeader()
+        {
             // Not sure if it would be more clever to do this as I read the file using CsvReader,
             // but I just want to make something that works in C# at the moment.
-            using (var reader = new StreamReader(CSV_SRC_PATH))
+            using (var reader = new StreamReader(this.CSV_SRC_PATH))
             {
                 string headerformat = "Seller,Product,Price,OrderDate,Branch";
                 string? firstline = reader.ReadLine();
@@ -65,24 +177,25 @@ namespace TmoTask.Controllers
                         throw new InvalidDataException(
                             "Input CSV file's header's field names are not RFC 4180 compliant");
                     }
-                 // Header has invalid field names.
+                // Header has invalid field names.
                     else throw new InvalidDataException(
-                        "Input CSV file has wrong field names in header");
+                        $"Input CSV file must have header {headerformat}");
                 }
             }
+        }
 
-            // Read each line from the CSV file. Return a JSON-formatted string with one key "list"
-            // whose value is an array of strings corresponding to the branch names.
+        private HashSet<string> ProcessCSVAndExtractBranchnames()
+        {
             HashSet<string> branchnames = new HashSet<string>();
-            using (var reader = new StreamReader(CSV_SRC_PATH))
+            using (var reader = new StreamReader(this.CSV_SRC_PATH))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 var parsed = csv.GetRecords<CsvHeaders>();
                 // Regex to check if values in the Price field are valid.
                 // Allow whole dollars to implicitly be zero (i.e. the pattern matches "0.99" and
-                // ".16") but there must be two decimal places, corresponding to cents (i.e. the
-                // pattern matches "22162.33" but not "0.113" or ".2").
-                string pricepattern = @"^\d*\.\d{2}$";
+                // ".16") but there must be 1+ decimal places, corresponding to cents (i.e. the
+                // pattern matches "22162.33" and ".2" but not "0.113").
+                string pricepattern = @"^\d*\.\d{1,2}$";
                 // Regex to check if values in the OrderDate field are valid. OrderDates must be in
                 // format YYYY-MM-DD.
                 string datepattern = @"\d{4}-\d\d-\d\d";
@@ -96,7 +209,7 @@ namespace TmoTask.Controllers
                     // I don't know how data in orders.csv might be organized in the future (i.e.
                     // what if someone inputs an employee name into the Seller field in the
                     // future?)
-                    MatchCollection pricematch = priceregex.Matches(line.Price);
+                    MatchCollection pricematch = priceregex.Matches(Convert.ToString(line.Price));
                     MatchCollection datematch = dateregex.Matches(line.OrderDate);
                     System.Diagnostics.Debug.Assert(pricematch.Count < 2, "Bad Price regex");
                     System.Diagnostics.Debug.Assert(datematch.Count < 2, "Bad OrderDate regex");
@@ -106,45 +219,54 @@ namespace TmoTask.Controllers
                         $"Invalid Price value '{line.Price}' in line: {linetext}");
                     if (datematch.Count == 0) throw new InvalidDataException(
                         $"Invalid OrderDate value '{line.OrderDate}' in line: {linetext}");
+                    string[] splitdate = line.OrderDate.Split('-');
+                    int yyyy = int.Parse(splitdate[0]);
+                    int mm = int.Parse(splitdate[1]);
+                    int dd = int.Parse(splitdate[2]);
+                    // Make sure month and day values are valid.
+                    if (mm < 1 || mm > 12 || dd < 1 || dd > 31) throw new InvalidDataException(
+                        $"Invalid OrderDate value '{line.OrderDate}' in line: {linetext}");
+                    switch (mm)
+                    {
+                        case 2:
+                            // Handle leap years: divisble by 4, but not 100, unless also divisible
+                            // by 400.
+                            int ddbound;
+                            if (yyyy % 4 == 0)
+                            {
+                                // Handle years divisible by 100
+                                if (yyyy % 100 == 0)
+                                    // divisible by 400? Then it's a leap year, otherwise no.
+                                    ddbound = (yyyy % 400 == 0) ? 29 : 28;
+                                // leap years divisible by 4 but not 100 are always leap years.
+                                else ddbound = 29;
+                            }
+                            else ddbound = 28;
+                            if (dd > ddbound) throw new InvalidDataException(
+                                $"Invalid OrderDate value '{line.OrderDate}' in line: {linetext}");
+                            else break;
+                        case 1:
+                        case 3:
+                        case 5:
+                        case 7:
+                        case 8:
+                        case 10:
+                        case 12:
+                            if (dd > 31) throw new InvalidDataException(
+                                $"Invalid OrderDate value '{line.OrderDate}' in line: {linetext}");
+                            else break;
+                        default:
+                            if (dd > 31) throw new InvalidDataException(
+                                $"Invalid OrderDate value '{line.OrderDate}' in line: {linetext}");
+                            break;
+                    }
                     branchnames.Add(line.Branch);
                 }
+                // We only get to this point if the CSV parsed successfully. Store the processed
+                // data from the CSV as a member object.
+                this._processeddata = parsed;
             }
-            string[] arr = new string[branchnames.Count];
-            branchnames.CopyTo(arr);
-            var result = new
-            {
-                list = arr
-            };
-            return Ok(result);
-        }
-
-        [HttpGet]
-        [Route("{branchname}/{numSellers}")]
-        public IActionResult GetTopSellers(string branchname, int numSellers)
-        {
-            /**************************************************************************************
-             * Returns an HTTP status code. If the query was successful, also return the top
-             * sellers.
-             * 
-             * NOTE: From my understanding of the technical requirements, I could easily move the
-             *       the functionality of this code into the GetBranches() function, and I suspect
-             *       that was why the source file provided as a template provided a single Get()
-             *       function that I could fill out. However, in a live environment, grabbing a
-             *       list of branches and grabbing the top sellers from each branch aren't two
-             *       functionalities that always occur together; what if someone wanted to grab the
-             *       list of branches and then sort by product instead, for example?
-             *       
-             *       Anyhow, the same caveats that apply to the GetBranches() function occur here.
-             *       This code would be problematic in an environment that handles large volumes
-             *       of data since I'm rereading the entire CSV "database" every time this function
-             *       is called, but I don't have the enterprise level experience needed to write
-             *       this code any cleaner. All I can do is note that caveat here. My guess is that
-             *       the results of GetBranch() would be cached somehow and that a boolean
-             *       parameter for whether a database refresh is needed would be used. That line of
-             *       reasoning lines up with what I've learned using the Meta JSSDK, at least.
-             * ***********************************************************************************/
-            string? branchlist = (string?)GetBranches().Value;
-
+            return branchnames;
         }
     }
 }
---
+++ TmoTaskTests/Controllers/PerformanceReportControllerTests.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TmoTask.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.TypeConversion;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace TmoTask.Controllers.Tests
+{
+    [TestClass()]
+    public class PerformanceReportControllerTests
+    {
+        // Not sure if this solution works across platforms but it feels better than hardcoding a
+        // path based on where I myself stored the project. Also more secure.
+        //
+        // Running the tests seems to occur in /{solution folder}/TmoTaskTests/bin/debug/net8.0
+        // but the test CSV files are stored in /{solution folder}/TmoTaskTests/testcases
+        private string CSVFOLDER = "../../../testcases";
+
+        [TestMethod()]
+        public void ValidateOriginalCSV()
+        {
+            var c = new PerformanceReportController();
+            var d = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/../orders.csv");
+            try
+            {
+                c.GetBranches();
+                c.GetTopSellers("Branch 3", 2);
+                c.GetTopSellers("Branch 2", 113);
+            }
+            catch (InvalidDataException)
+            {
+                // We shouldn't run into this code when we made a valid query into the CSV
+                // "database".
+                Assert.Fail();
+            }
+        }
+
+        // This method shouldn't throw an exception if our logic in GetTopSellers() worked. The
+        // logic should force GetBranches() to run if we haven't processed the CSV data yet.
+        [TestMethod()]
+        public void TestCSVAvoidUninitiatedDataOnGetTopSeller()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/../orders.csv");
+            c.GetTopSellers("Branch 1", 22, false);
+        }
+
+        [TestMethod()]
+        public void TestEmptyCSV()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/empty.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void TestNewLineOnlyCSV()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/newline.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVHeaderTooFewFieldsWrongFieldNames()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/badheader1.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVHeaderTooManyFieldsWrongFieldNames()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/badheader2.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVHeaderMissingFieldNames()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/badheader3.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVHeaderTooManyFieldNames()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/badheader4.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVHeaderViolatesRFC4180()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/badheader5.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataPriceNoDecimal()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata1.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        //[TestMethod()]
+        //public void CSVDataPriceTooFewDecimals()
+        //{
+        //    var c = new PerformanceReportController();
+        //    c._setcsvpath($"{CSVFOLDER}/baddata2.csv");
+        //    Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        //}
+
+        [TestMethod()]
+        public void CSVDataPriceTooManyDecimals()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata3.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataPriceIsAlphaString()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata4.csv");
+            Assert.ThrowsException<TypeConverterException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataPriceIsSpecialChar()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata5.csv");
+            Assert.ThrowsException<TypeConverterException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataPriceIsNegative()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata6.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateBadFormat()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata7.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateYearTooShort()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata8.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateYearTooLong()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata9.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateMalformedMissingMonth()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata10.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateSingleDigitMonth()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata11.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateValidMonthTooLong()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata12.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateIllegalMonth()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata13.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        // Probably no need to check formatting issue for days since if our regex is bad, it
+        // probably failed the tests for formatting issues for months and years.
+
+        [TestMethod()]
+        public void ValidateLeapYearLogic()
+        {
+            var c = new PerformanceReportController();
+            try
+            {
+                c._setcsvpath($"{CSVFOLDER}/leapyears.csv");
+                c.GetBranches();
+            }
+            catch (InvalidDataException)
+            {
+                // We shouldn't ever catch this exception because the file has good leap years.
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateBadLeapYearNotMultOf4()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata14.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void CSVDataOrderDateBadLeapYearMultOf100()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata15.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        // We don't need to check over 31 days since no month has over 31 days.
+        public void CSVDataOrderDateBad30DayMonth()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/baddata16.csv");
+            Assert.ThrowsException<InvalidDataException>(() => c.GetBranches());
+        }
+
+        [TestMethod()]
+        public void QueryNonExistentBranchName()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/../orders.csv");
+            var res = c.GetTopSellers("foo", 1000, false);
+            Assert.Equals(typeof(NotFound), res.GetType());
+        }
+
+        [TestMethod()]
+        public void QueryNumSellerLessThanOne()
+        {
+            var c = new PerformanceReportController();
+            c._setcsvpath($"{CSVFOLDER}/../orders.csv");
+            var res = c.GetTopSellers("Branch 3", 0);
+            Assert.Equals(typeof(BadRequest), res.GetType());
+            res = c.GetTopSellers("Branch 1", -3);
+            Assert.Equals(typeof(BadRequest), res.GetType());
+        }
+    }
+}
